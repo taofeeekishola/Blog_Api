feb0ff18615fb7d8f879c7555b95f6422e76161e
taofeeekishola <taofeekishola1791@gmail.com>
2025-03-11 00:27:03 +0100

    create IComment and CommentRepository

---
+++ DataAccessLayer/IRepository/IComment.cs
@@ -0,0 +1,45 @@
+﻿using DomainLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.IRepository
+{
+    public interface IComment
+    {
+        /// <summary>
+        /// Create a comment
+        /// </summary>
+        /// <param name="comment"></param>
+        /// <returns>Object of Comment</returns>
+        Comment Create(Comment comment);
+
+        /// <summary>
+        /// Get all Comments
+        /// </summary>
+        /// <returns>List of Comment</returns>
+        List<Comment> GetAll();
+
+        /// <summary>
+        /// Get a Comment
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Object of Comment</returns>
+        Comment? Get(int id);
+
+        /// <summary>
+        /// Update Comment
+        /// </summary>
+        /// <param name="comment"></param>
+        /// <returns></returns>
+        Comment? Update(Comment comment);
+
+        /// <summary>
+        /// Delete comment
+        /// </summary>
+        /// <param name="comment"></param>
+        void Delete(Comment comment);
+    }
+}
---
+++ DataAccessLayer/Repository/CommentRepository.cs
@@ -0,0 +1,65 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.IRepository;
+using DomainLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class CommentRepository : IComment
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CommentRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        //functiont to create a comment
+        public Comment Create(Comment comment)
+        {
+            _applicationDbContext.Add(comment);
+            _applicationDbContext.SaveChanges();
+
+            return comment;
+        }
+
+        //function to delete a comment
+        public void Delete(Comment comment)
+        {
+            _applicationDbContext.Remove(comment);
+            _applicationDbContext.SaveChanges();
+        }
+
+        //function to get a single comment
+        public Comment? Get(int id)
+        {
+            Comment? comment = _applicationDbContext.Comments.Find(id);
+
+            return comment;
+        }
+
+        //function to get all comments
+        public List<Comment> GetAll()
+        {
+            return _applicationDbContext.Comments.ToList();
+        }
+
+        //function to update a comment
+        public Comment? Update(Comment comment)
+        {
+            Comment? existingComment = _applicationDbContext.Comments.Find(comment.Id);
+
+            existingComment.Content = comment.Content;
+
+            _applicationDbContext.Comments.Update(existingComment);
+            _applicationDbContext.SaveChanges();
+
+            return existingComment;
+ 
+        }
+    }
+}
